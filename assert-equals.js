1be263ea63fefcdf60782e2fc8974d92dd2ad92a
GeorgeKlemperer <george@klemperer.co.uk>
2023-07-19 23:31:12 +0100

    v1 strings and numbers working

--- assert-equals.js
+++ assert-equals.js
@@ -1,3 +1,17 @@
-function assertEquals(expect, actual) {}
+function assertEquals(expected, actual) {
+  if (expected === actual) {
+    console.log(
+      `Assertion passed: Expected and actual values are equal (${expected} = ${actual}).`
+    );
+    return true;
+  } else {
+    console.log(
+      `Assertion failed: Expected ${expected}, but returned ${actual}.`
+    );
+    throw new Error(
+      `Assertion failed: Expected ${expected}, but returned ${actual}.`
+    );
+  }
+}
 
-module.exports = assertEquals
+module.exports = assertEquals;
--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -1,13 +1,37 @@
-const assertEquals = require('./assert-equals')
+const assertEquals = require("./assert-equals");
 
-describe('assertEquals', () => {
-  describe('when expected and actual are the same string', () => {
-    it('returns without throwing an error', () => {
-      expect(() => assertEquals('abc', 'abc')).not.toThrow()
-    })
-  })
+describe("assertEquals", () => {
+  describe("when expected and actual are the same string", () => {
+    it("returns without throwing an error", () => {
+      expect(() => assertEquals("abc", "abc")).not.toThrow();
+    });
+  });
 
-  describe('when expected and actual are different strings', () => {
-    it.todo('throws an error')
-  })
-})
+  describe("when expected and actual are different strings", () => {
+    it("throws an error", () => {
+      // Call assertEquals with different string arguments and expect it to throw an error
+      expect(() => assertEquals("abc", "def")).toThrow();
+    });
+  });
+
+  describe("when expected and actual are the same number", () => {
+    it("returns without throwing an error", () => {
+      expect(() => assertEquals(42, 42)).not.toThrow();
+    });
+  });
+
+  describe("when expected and actual are different number", () => {
+    it("throws an error", () => {
+      // Call assertEquals with different string arguments and expect it to throw an error
+      expect(() => assertEquals(42, 43)).toThrow();
+    });
+  });
+
+  describe("when expected and actual are the same array", () => {
+    it("returns without throwing an error", () => {
+      expect(() =>
+        assertEquals([1, 1984, 0, 1882, 42], [1, 1984, 0, 1882, 42])
+      ).not.toThrow();
+    });
+  });
+});
