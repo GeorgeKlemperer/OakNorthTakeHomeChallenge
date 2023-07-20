b62c94f8305de6b900f2aeae13d4e6bf81b29d31
GeorgeKlemperer <george@klemperer.co.uk>
2023-07-20 17:35:09 +0100

    test added: typeof

--- assert-equals.spec.js
+++ assert-equals.spec.js
@@ -27,50 +27,46 @@ describe("assertEquals", () => {
     });
   });
 
+  describe("when expected and actual are different data types", () => {
+    it("throws an error", () => {
+      // Call assertEquals with different data types and expect it to throw an error
+      expect(() => assertEquals(1, "1")).toThrow();
+    });
+  });
+
   describe("when expected and actual are the same boolean", () => {
     it("returns without throwing an error", () => {
       expect(() => assertEquals(true, true)).not.toThrow();
     });
   });
 
-  describe("when expected and actual are different boolean", () => {
+  describe("when expected and actual are different booleans", () => {
     it("throws an error", () => {
       // Call assertEquals with different boolean arguments and expect it to throw an error
       expect(() => assertEquals(true, false)).toThrow();
     });
   });
 
-  describe("when expected and actual are both undefined", () => {
-    it("returns without throwing an error", () => {
-      expect(() => assertEquals(undefined, undefined)).not.toThrow();
-    });
-  });
-
-  describe("when only one of expected and actual is undefined", () => {
-    it("throws an error", () => {
-      // Call assertEquals with a defined and a undefined argument and expect it to throw an error
-      expect(() => assertEquals(true, undefined)).toThrow();
-    });
-  });
-
-  describe("when expected and actual are both null", () => {
-    it("returns without throwing an error", () => {
-      expect(() => assertEquals(null, null)).not.toThrow();
-    });
-  });
-
-  describe("when only one of expected and actual is null", () => {
-    it("throws an error", () => {
-      // Call assertEquals with a defined and a undefined argument and expect it to throw an error
-      expect(() => assertEquals(null, undefined)).toThrow();
-    });
-  });
-
   describe("when expected and actual are the same array", () => {
     it("returns without throwing an error", () => {
       expect(() =>
-        assertEquals([-1, 1984, 0, -1882, 42], [-1, 1984, 0, -1882, 42])
+        assertEquals(
+          [-1, 1984, "a", "b", "c", 0, -1882, 42],
+          [-1, 1984, "a", "b", "c", 0, -1882, 42]
+        )
       ).not.toThrow();
+    });
+  });
+
+  describe("when expected and actual are different arrays", () => {
+    it("throws an error", () => {
+      // Call assertEquals with different array arguments and expect it to throw an error
+      expect(() =>
+        assertEquals(
+          [-1, 1984, "a", "b", "c", 0, -1882, 42],
+          [-10, 4819, "d", "e", "f", 0, 1882, 42]
+        )
+      ).toThrow();
     });
   });
 });
