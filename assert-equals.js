f558efaf830306bb62d0071c7650a06feb68306d
GeorgeKlemperer <george@klemperer.co.uk>
2023-07-20 17:51:02 +0100

    assertEquals now passes array test

--- assert-equals.js
+++ assert-equals.js
@@ -1,13 +1,31 @@
 function assertEquals(expected, actual) {
-  if (expected === actual) {
-    console.log(
-      `Assertion passed: Expected and actual values are equal (${expected} = ${actual}).`
+  if (typeof expected !== typeof actual) {
+    throw new Error(
+      `Assertion failed: Expected type ${typeof expected} but got ${typeof actual}`
     );
+  }
+
+  if (typeof expected === "object") {
+    if (expected.length !== actual.length) {
+      throw new Error(
+        `Assertion failed: Expected array length ${expected.length} but got ${actual.length}`
+      );
+    }
+
+    for (let i = 0; i < expected.length; i++) {
+      if (expected[i] !== actual[i]) {
+        throw new Error(
+          `Assertion failed: Expected ${expected[i]} but got ${actual[i]}`
+        );
+      }
+    }
+
+    return true; // All elements in the array are equal
+  }
+
+  if (expected === actual) {
     return true;
   } else {
-    console.log(
-      `Assertion failed: Expected ${expected}, but returned ${actual}.`
-    );
     throw new Error(
       `Assertion failed: Expected ${expected}, but returned ${actual}.`
     );
